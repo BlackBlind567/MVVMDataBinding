c43f9750cd61c2fb161300bf6703b164ad19e5af
RBM-Mall <78599084+RBM-Mall@users.noreply.github.com>
2023-03-30 19:11:04 +0530

    stage2

---
+++ app/src/main/java/com/mvvm/myapplicationmvvm/CategoryAdapter.java
@@ -0,0 +1,35 @@
+package com.mvvm.myapplicationmvvm;
+
+import android.content.Context;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mvvm.myapplicationmvvm.databinding.CategoryLayoutBinding;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends BlindAdapter<CategoryData, CategoryLayoutBinding>{
+    public CategoryAdapter(Context context, ArrayList<CategoryData> arrayList) {
+        super(context, arrayList);
+    }
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.category_layout;
+    }
+
+    @Override
+    public void onBindData(CategoryData model, int position, CategoryLayoutBinding dataBinding) {
+        dataBinding.text.setText(model.getName());
+    }
+
+    @Override
+    protected void onBindDataHolder(CategoryData categoryData, int position, CategoryLayoutBinding mDataBinding, RecyclerView.ViewHolder holder) {
+
+    }
+
+    @Override
+    public void onItemClick(CategoryData model, int position) {
+
+    }
+}
